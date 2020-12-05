593389445d919c6707b92a9b5d2a9684e312235d
Helio Chissini de Castro <helio@kde.org>
2020-12-05 11:15:31 +0100

    Prepare to unify the advent code

--- 2019/day-3/src/main.rs
+++ 2019/day-3/src/main.rs
@@ -1,9 +1,11 @@
 extern crate clap;
+extern crate colored;
 
 use clap::{Arg, App};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::mem;
+use colored::*;
 
 fn main() {
     let input_arguments = App::new("Advent Day Two")
@@ -28,7 +30,7 @@ fn main() {
     }
     
     let res = process_program(data[0].clone(), data[1].clone());
-    println!("Resulting data:{:?}", res);
+    println!("Distance:{:?} Steps:{:?}", res.0, res.1);
 }
 
 #[cfg(test)]
@@ -36,28 +38,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_day_03() {
+    fn test_01() {
         assert_eq!(process_program(
             String::from("R8,U5,L5,D3"),
             String::from("U7,R6,D4,L4")),
-        6);
+        (6, 30));
+    }
+    #[test]
+    fn test_02() {
         assert_eq!(process_program(
             String::from("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
             String::from("U62,R66,U55,R34,D71,R55,D58,R83")),
-        159);
+        (159,610));
+    }
+    #[test]
+    fn test_03() {
         assert_eq!(process_program(
             String::from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
             String::from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")),
-        135);
+        (135,410));
     } 
 }
 
 
-fn process_program(data_a:String, data_b:String) -> i32 {
+fn process_program(data_a:String, data_b:String) -> ( i32, i32 ) {
     let mut points:Vec<(i32,i32)> = Vec::new();
     let wires: Vec<String> = vec![data_a, data_b];
-    let mut res = 0;
+    let mut minimal_distance = 0;
+    let mut minimal_steps = 0;
     let mut wiresize: Vec<usize> = Vec::new();
+
+    println!("{}", "Processing wire program ( Advent 2019 - Day 3 )".yellow().bold());
 
     for wire in wires {
         let path: Vec<String> = wire.split(',').map(|x| x.parse::<String>().unwrap()).collect();
@@ -84,11 +95,10 @@ fn process_program(data_a:String, data_b:String) -> i32 {
                     },
                 _ => println!("Invalid direction"),
             }
-
-            // println!("{:?}", points);
         }
     }
 
+    let mut steps_a = 0;
     for j in 1..=wiresize[0] {
         let ( mut amin, mut amax, a_fixed, a_axis ) = if points[j-1].0 == points[j].0 { 
             (points[j-1].1, points[j].1, points[j].0, true) 
@@ -96,30 +106,52 @@ fn process_program(data_a:String, data_b:String) -> i32 {
             (points[j-1].0, points[j].0, points[j].1, false) 
         };
         if amax < amin { mem::swap(&mut amax, &mut amin) }
+        println!("{} <=> {}", amax, amin);
+        if amin < 0 {
+            steps_a = steps_a + (amax + amin.abs());
+        } else {
+            steps_a = steps_a + (amax - amin);
+        }
         
+        let mut steps_b = 0;
         for k in wiresize[0] + 2..=wiresize[0] + wiresize[1] + 1 {
             let (mut min, mut max, b_fixed, b_axis) = if points[k-1].0 == points[k].0 {
                 (points[k-1].1, points[k].1, points[k].0, true)
             } else {
                 (points[k-1].0, points[k].0, points[k].1, false)
             };
+            
             if max < min { mem::swap(&mut max, &mut min) }
+            if min < 0 {
+                steps_b = steps_b + (max + min.abs());
+            } else {
+                steps_b = steps_b + (max - min);
+            }
 
             if a_axis != b_axis {
-                if a_fixed >= min && a_fixed <= max && b_fixed >= amin && b_fixed <= amax {
+                if  a_fixed >= min
+                    && a_fixed <= max
+                    && b_fixed >= amin
+                    && b_fixed <= amax
+                    && a_fixed + b_fixed != 0 {
                     let sum = a_fixed.abs() + b_fixed.abs();
-                    if res == 0 || res > sum {
-                        res = sum;
+                    if minimal_distance == 0 || minimal_distance > sum {
+                        minimal_distance = sum;
                     }
+                    
                     if ! b_axis {
-                        println!("---------------- {:?},{:?} ", a_fixed, b_fixed);
+                        let ( a, b ) = (steps_a.abs() - (b_fixed - amin), steps_b - (a_fixed - min));
+                        if minimal_steps == 0 || minimal_steps > a+b { minimal_steps = a+b }
+                        // println!("a={:?} b={:?}, {}{:?}", a_fixed, b_fixed, "Res=".green(), a+b);
                     } else {
-                        println!("---------------- {:?},{:?} ", b_fixed, a_fixed);
+                        let (a, b) = (steps_a.abs() - (b_fixed - amin), steps_b - ( max - a_fixed));
+                        if minimal_steps == 0 || minimal_steps > a+b { minimal_steps = a+b }
+                        // println!("a={:?} b={:?}, {}{:?}", a_fixed, b_fixed, "Res=".green(), minimal_steps);
                     }
                 }
             }
         }
     }
 
-    return res;
+    return ( minimal_distance, minimal_steps );
 }
